376f8de81e157999c49c13d0b7cfc34626f115d8
Nathan Wendling <bug.new@gmail.com>
2018-09-06 16:27:08 -0700

    verified the other operations

--- calc.py
+++ calc.py
@@ -1,3 +1,12 @@
 a = "0"
 b = 2
 print( int(a) + b )		#cast a to an int before printing to avoid overloaded '+'
+
+#the rest of the basic operators copypasta'd
+print (3 + 4)
+print (3 - 4)
+print (3 * 4)
+print (3 / 4)
+print (3 % 2)
+print (3 ** 4) # 3 to the fourth power
+print (4 // 3) #floor division
