627c771b6f5e4d4bb5ba46f8711fe9ab8313025e
Nathan Wendling <bug.new@gmail.com>
2018-09-07 13:54:37 -0700

    cleaned up construction code and removed incorrect comments

--- adderclass.py
+++ adderclass.py
@@ -2,11 +2,11 @@
 class adder(object):
 	#def __init__ (self):		#none of that overloading in python, y'hear me?
 	#	self.value = 0
-	def __init__ (self, val=None):
+	def __init__ (self, val=None):		#use default values to simulate overriding however
 		if val != None:
-			self.value = val		#non-functional?
+			self.value = val
 		else:
-			self.value=0			#seemingly always called, with or without val being declared
+			self.value=0
 	def add (self, val):
 		self.value += val
 	def getValue(self):
--- adderdriver.py
+++ adderdriver.py
@@ -10,11 +10,11 @@ while loop == True:
 	print("Value to manipulate? ")	
 	valu = input()
 	try:
-		valu=int(valu)
+		valu=int(valu) #if user input a non-integer, an exception will be thrown here, going to except
 		loop=False
 	except:
 		print("please input an integer")
 		continue
-simplecalc2 = adder(val=valu)
+simplecalc2 = adder(valu)
 simplecalc2.add(42)
 print("after manipulation the number is: %f" % simplecalc2.getValue())
