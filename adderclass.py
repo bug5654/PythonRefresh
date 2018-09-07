d2a12fa18010697ebaa62bc584d0d5c7cd55edf7
Nathan Wendling <bug.new@gmail.com>
2018-09-06 22:02:21 -0700

    added input and minor exception handlling

--- adderclass.py
+++ adderclass.py
@@ -1,7 +1,12 @@
 #AdderClass
 class adder(object):
-	def __init__ (self):
-		self.value = 0
+	#def __init__ (self):		#none of that overloading in python, y'hear me?
+	#	self.value = 0
+	def __init__ (self, val=None):
+		if val != None:
+			self.value = val		#non-functional?
+		else:
+			self.value=0			#seemingly always called, with or without val being declared
 	def add (self, val):
 		self.value += val
 	def getValue(self):
--- adderdriver.py
+++ adderdriver.py
@@ -3,3 +3,18 @@ from adderclass import *
 simplecalc = adder();
 simplecalc.add(2)
 print(simplecalc.getValue())
+
+loop=True
+valu=None
+while loop == True:
+	print("Value to manipulate? ")	
+	valu = input()
+	try:
+		valu=int(valu)
+		loop=False
+	except:
+		print("please input an integer")
+		continue
+simplecalc2 = adder(val=valu)
+simplecalc2.add(42)
+print("after manipulation the number is: %f" % simplecalc2.getValue())
