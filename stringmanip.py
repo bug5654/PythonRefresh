2a198f7f473050e167e24596f8a126a4c2899d41
Nathan Wendling <bug.new@gmail.com>
2018-09-06 18:24:18 -0700

    string manipulation parts added

--- stringmanip.py
+++ stringmanip.py
@@ -3,11 +3,14 @@ stringVar = "  Greetings Program!  "
 stringVarReset = stringVar
 
 print("string in question: '" + stringVar + "'")
+print( "type: " + str( type(stringVar) ) )
 print( "number of 'e's: " + str(stringVar.count("e")) )
-print( "position of 'P' " + str(stringVar.find('P')) )
+print( "position of 'P': " + str(stringVar.find('P')) )
 #confirmed: 0-based string position (C-style)
 print( "lowercase: " + stringVar.lower() )
 print( "uppercase: " + stringVar.upper() )
 print( "e->q: " + stringVar.replace('e','q') )
 stringVar = stringVarReset
 print( "strip whitespace: " + stringVar.strip() )
+print( "positions 3-5: " + stringVar[3:5])
+print( "remove last 3 characters: " + stringVar[:-3] )
